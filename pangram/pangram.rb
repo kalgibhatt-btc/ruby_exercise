50380a2e8a3068a3427afe2a6f207ce809742f96
kalgibhatt-btc <kalgi95.bhatt@gmail.com>
2017-01-19 21:43:19 +0530

    Update pangram.rb
    change done

--- pangram/pangram.rb
+++ pangram/pangram.rb
@@ -1,18 +1,16 @@
 class Pangram
-  def self.pangram?(input)
-    input=input.downcase
-  #  input=input.split
-    count=0
-
+  def self.pangram?(str)
+    input = str.downcase!
+    count = 0
     ('a'..'z').each do |i|
       if (input.include?i)
-        count=count+1
+        count += 1
       else
         return false
       end
     end
-    if(count==26)
-      returnt=true
+    if(count == 26)
+      returnt true
     end
   end
 end
