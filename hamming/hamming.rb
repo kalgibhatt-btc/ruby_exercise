cb73af38da55f8dfd7c1b87d974eb69342f9f4f9
kalgibhatt-btc <kalgi95.bhatt@gmail.com>
2017-01-20 16:06:26 +0530

    Update hamming.rb
    changed

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,23 +1,20 @@
 class Hamming
       def self.compute(string1,string2)
-
-      if string1.length!=string2.length
+      if string1.length != string2.length
         raise ArgumentError
-
       else
-        count=0
-        l1=string1.length
-        string1=string1.upcase!
-        string2=string2.upcase!
-        str1=string1.split('')
-        str2=string2.split('')
-        (0..l1).each do|i|
-          if(str1[i]!=str2[i])
-            count=count+1
+        count = 0
+        l1 = string1.length
+        string1 = string1.upcase!
+        string2 = string2.upcase!
+        str1 = string1.split('')
+        str2 = string2.split('')
+        (0..l1).each do |i|
+          if(str1[i] != str2[i])
+            count += 1
           end
-
       end
-      return count
+      count
       end
     end
 end
