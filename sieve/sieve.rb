8755be170e89b746cd2065d985347dbdcfecc952
kalgibhatt-btc <kalgi95.bhatt@gmail.com>
2017-01-23 14:55:24 +0530

    First commit Sieve

---
+++ sieve/sieve.rb
@@ -0,0 +1,15 @@
+require 'prime'
+class Sieve
+  def initialize(number)
+    @input = number
+  end
+  def primes
+    array_of_primes = []
+    return [] if @input == 1
+    return [2] if @input == 2
+    Prime.each(@input) do |i|
+      array_of_primes.push(i)
+    end
+      array_of_primes
+  end
+end
