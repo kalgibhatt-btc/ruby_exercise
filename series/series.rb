93ab369d5a4bf46bfe8c119646c01804f1bfac9e
kalgibhatt-btc <kalgi95.bhatt@gmail.com>
2017-01-25 11:55:17 +0530

    First commit Series

---
+++ series/series.rb
@@ -0,0 +1,21 @@
+class Series
+  def initialize(number)
+    @number = number
+    @array_number = number.split('')
+  end
+  def slices(n)
+    result = Array.new()
+    length = @number.length
+    raise ArgumentError if n > length
+    result = @array_number if n == 1
+    # (0..length-1).each do |i|
+    # result[] = @number[i] + @number[i+1]
+    # i+= 1
+    # break if @number[i].length == n
+    # end
+    (0...length-n+1).each do |i|
+      result.push(@number.slice(i,n).to_s)
+    end
+    result.uniq
+  end
+end
